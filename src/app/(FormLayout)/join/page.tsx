ab73afae694f41ebdadf25f3f69688dea451b431
JIMIN1020 <jimins4920@gmail.com>
2024-06-01 16:45:14 +0900

    [Feat/#1]: 에러 팝업 구현

--- src/app/(FormLayout)/join/page.tsx
+++ src/app/(FormLayout)/join/page.tsx
@@ -1,6 +1,6 @@
 import FormHeader from '@/components/common/header/FormHeader';
+import ErrorPopUp from '@/components/form/ErrorPopUp';
 import FormInput from '@/components/form/FormInput';
-import TermsPopUp from '@/components/form/TermsPopUp';
 import React from 'react';
 
 function JoinPage() {
@@ -12,7 +12,7 @@ function JoinPage() {
         errorMsg='에러 메시지'
         placeholder='비밀번호 입력'
       />
-      <TermsPopUp />
+      <ErrorPopUp />
     </div>
   );
 }
--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -3,5 +3,6 @@ const BASE_PATH = '/icons';
 export const ICONS = {
   form: {
     backBtn: `${BASE_PATH}/form/back-btn.svg`,
+    error: `${BASE_PATH}/form/error-icon.svg`,
   },
 };
