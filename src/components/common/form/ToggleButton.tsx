ca696bc5faff21068d7f961a1986291ad709d553
JIMIN1020 <jimins4920@gmail.com>
2024-06-03 22:02:50 +0900

    [Feat/#9]: 공개 토글 버튼 구현

---
+++ src/components/common/form/ToggleButton.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { useFormContext } from 'react-hook-form';
+
+interface Props {
+  fieldName: 'job' | 'gender' | 'birthDate';
+}
+
+function ToggleButton({ fieldName }: Props) {
+  const [isPublic, setIsPublic] = useState<boolean>(false);
+  const { setValue } = useFormContext();
+
+  useEffect(() => {
+    if (!isPublic) {
+      setValue(fieldName, null);
+    }
+  }, [isPublic]);
+
+  return (
+    <div className='flex gap-[4px]'>
+      <span className='text-body_md text-white'>
+        {isPublic ? '공개' : '비공개'}
+      </span>
+      <input
+        type='checkbox'
+        checked={isPublic}
+        onChange={(e) => setIsPublic(e.target.checked)}
+        className='relative h-[20px] w-[40px] cursor-pointer appearance-none rounded-[20px] bg-gray-700 outline-none before:absolute before:left-[2px] before:top-[2px] before:h-[16px] before:w-[16px] before:rounded-[50%] before:bg-white before:transition-transform before:duration-200 checked:bg-main checked:before:translate-x-[20px]'
+      />
+    </div>
+  );
+}
+
+export default ToggleButton;
