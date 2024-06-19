52b70900e47a1949a1bdd4e58d080eb5edce5a9d
JIMIN1020 <jimins4920@gmail.com>
2024-06-19 15:20:28 +0900

    [File/#22]: instance 파일 삭제

--- src/api/instances/publicInstance.ts
+++
@@ -1,8 +0,0 @@
-import { BASE_URL } from '@/constants/api';
-import axios from 'axios';
-
-axios.defaults.timeout = 5000;
-
-export const publicInstance = axios.create({
-  baseURL: BASE_URL,
-});
--- src/api/interceptors/interceptor.ts
+++
@@ -1,5 +0,0 @@
-import { AxiosResponse } from 'axios';
-
-export const onResponse = (response: AxiosResponse) => {
-  return response;
-};
