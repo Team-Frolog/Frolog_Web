0f28f80cbbc124bddcbc18685f651c55a2c11c19
JIMIN1020 <jimins4920@gmail.com>
2024-06-25 14:14:57 +0900

    [Feat/#24]: 추천 도서 더미데이터 추가

--- src/components/common/book/BookItem.tsx
+++ src/components/common/book/BookItem.tsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import Tag from '../tag/Tag';
 import Rating from '../rating/Rating';
+import { BooksByType } from '../../../data/dummy/booksByType';
 
-function BookItem() {
+interface Props {
+  bookData: BooksByType;
+}
+
+function BookItem({ bookData }: Props) {
   return (
     <div className='flex w-full gap-[20px] text-gray-800'>
       <div className='h-[170px] w-[120px] bg-gray-400'>image</div>
       <div className='flex flex-1 flex-col gap-[16px]'>
         <div className='flex flex-col gap-[4px]'>
-          <h5 className='text-body_xl_bold'>메리</h5>
-          <span className='text-caption_bold text-gray-600'>안녕달</span>
+          <h5 className='text-body_xl_bold'>{bookData.name}</h5>
+          <span className='text-caption_bold text-gray-600'>
+            {bookData.author}
+          </span>
         </div>
         <div className='flex flex-col gap-[8px]'>
-          <Rating rating={4} />
+          <Rating rating={bookData.rating} />
           <div className='flex flex-col gap-[4px]'>
-            <Tag type='pros' text='술술 읽혀요' />
-            <Tag type='cons' text='다른 비슷한 책이 더 나아요' />
+            {bookData.totalReviews > 0 ? (
+              <>
+                <Tag type='pros' text='술술 읽혀요' />
+                <Tag type='cons' text='다른 비슷한 책이 더 나아요' />
+              </>
+            ) : (
+              <Tag type='default' />
+            )}
           </div>
-          <span className='text-caption_bold text-gray-600'>총 0개의 리뷰</span>
+          <span className='text-caption_bold text-gray-600'>
+            총 {bookData.totalReviews}개의 리뷰
+          </span>
         </div>
       </div>
     </div>
