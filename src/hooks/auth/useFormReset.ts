02f3f04b8609c873b187bd5023eb51852d8e056b
JIMIN1020 <jimins4920@gmail.com>
2024-06-26 17:12:58 +0900

    [Fix/#27]: form reset 로직 수정

--- src/hooks/auth/useFormReset.ts
+++ src/hooks/auth/useFormReset.ts
@@ -11,23 +11,24 @@ import { useEffect } from 'react';
 
 export const useFormReset = () => {
   const { data: session } = useSession();
+
+  if (session) return null;
+
   const { resetFindStep, resetJoinStep, resetTestStep } = useStepActions();
   const pathname = usePathname();
 
   useEffect(() => {
-    if (!session) {
-      if (pathname !== PAGES.JOIN) {
-        localStorage.removeItem(JOIN_FORM_KEY);
-        resetJoinStep();
-      }
-      if (pathname !== PAGES.FIND_PASSWORD) {
-        localStorage.removeItem(FIND_FORM_KEY);
-        resetFindStep();
-      }
-      if (pathname !== PAGES.TEST) {
-        localStorage.removeItem(TEST_ANSWER_KEY);
-        resetTestStep();
-      }
+    if (pathname !== PAGES.JOIN) {
+      localStorage.removeItem(JOIN_FORM_KEY);
+      resetJoinStep();
     }
-  }, [pathname]);
+    if (pathname !== PAGES.FIND_PASSWORD) {
+      localStorage.removeItem(FIND_FORM_KEY);
+      resetFindStep();
+    }
+    if (pathname !== PAGES.TEST) {
+      localStorage.removeItem(TEST_ANSWER_KEY);
+      resetTestStep();
+    }
+  }, [pathname, session]);
 };
