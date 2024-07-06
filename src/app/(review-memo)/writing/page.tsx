03cbdfa50b0da82d3be9cc6047858cc2e8787187
JIMIN1020 <jimins4920@gmail.com>
2024-07-06 22:34:45 +0900

    [Feat/#33]: 리뷰 작성 - 레이아웃

--- src/app/(review-memo)/writing/page.tsx
+++ src/app/(review-memo)/writing/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function WritingPage() {
-  return <div>WritingPage</div>;
+  return (
+    <div className='flex h-fit w-full flex-1 flex-col items-center bg-white text-gray-800'>
+      <div>도서 컴포넌트</div>
+      <div>별점 컴포넌트</div>
+      <div>태그 컴포넌트</div>
+      <div>한줄평</div>
+      <div>리뷰</div>
+      <div>완료 버튼</div>
+    </div>
+  );
 }
 
 export default WritingPage;
