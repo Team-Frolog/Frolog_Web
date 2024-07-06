1bdc1c4eea6925a4c026d7b9dc7c8a9a92d1f6cf
JIMIN1020 <jimins4920@gmail.com>
2024-07-06 19:30:22 +0900

    [Test/#32]: theme test

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -2,15 +2,29 @@
 
 import { DARK_PAGE } from '@/constants/page';
 import { usePathname } from 'next/navigation';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function ThemeProvider() {
   const pathname = usePathname();
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const body = document.querySelector('body')!;
-      body.style.backgroundColor = DARK_PAGE.includes(pathname)
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && windowWidth < 450) {
+      const html = document.querySelector('html')!;
+      html.style.backgroundColor = DARK_PAGE.includes(pathname)
         ? '#0E0E0E'
         : '#FFFFFF';
     }
