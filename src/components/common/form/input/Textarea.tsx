e3c097676971db8c0465610c28b52fc68813bdd4
JIMIN1020 <jimins4920@gmail.com>
2024-07-08 11:16:55 +0900

    [Feat/#33]: dynamic input 구현

--- src/components/common/form/input/Textarea.tsx
+++ src/components/common/form/input/Textarea.tsx
@@ -9,12 +9,24 @@ function Textarea({ option }: Props) {
   return (
     <div className='flex w-full flex-col gap-[8px]'>
       <span className='text-body_md text-gray-700'>{option.title}</span>
-      <textarea
-        className='textarea-default h-fit min-h-[40px]'
-        maxLength={option.maxLength}
-        placeholder={option.placeholder}
-        rows={option.minRow}
-      />
+      <div
+        className="grid
+        after:invisible after:w-full after:whitespace-pre-wrap after:break-all after:border after:px-[16px] after:py-[18px] after:text-inherit after:content-[attr(data-cloned-val)_'_'] after:[grid-area:1/1/2/2]
+        [&>textarea]:resize-none [&>textarea]:overflow-hidden [&>textarea]:[grid-area:1/1/2/2]"
+      >
+        <textarea
+          className='textarea-default'
+          maxLength={option.maxLength}
+          placeholder={option.placeholder}
+          onInput={(e) => {
+            const target = e.target as HTMLTextAreaElement;
+            const parentNode = target.parentNode as HTMLElement;
+            parentNode.dataset.clonedVal = target.value;
+          }}
+          rows={option.minRow}
+        />
+      </div>
+
       <div className='flex justify-between'>
         <span className='text-body_md text-error'>{option.errorMessage}</span>
         <span className='text-body_md text-gray-700'>4/{option.maxLength}</span>
