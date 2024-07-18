9a3205792ae1d6b03b871939d982958d4fe77c21
JIMIN1020 <jimins4920@gmail.com>
2024-07-18 20:50:25 +0900

    [Feat/#34]: 리뷰 수정 페이지 연결

--- src/components/common/review/ReviewListItem.tsx
+++ src/components/common/review/ReviewListItem.tsx
@@ -1,25 +1,34 @@
 'use client';
 
 import React from 'react';
+import { useRouter } from 'next/navigation';
 import { usePopUpActions } from '@/store/popUpStore';
 import Rating from '../rating/Rating';
 import TagSlider from './TagSlider';
 
 function ReviewListItem() {
+  const router = useRouter();
   const { changePopUpState } = usePopUpActions();
 
   return (
     <div className='flex w-full flex-col gap-[20px] rounded-[20px] bg-white px-[24px] pt-[36px]'>
-      <div className='flex w-full cursor-pointer flex-col gap-[12px] text-gray-800'>
-        <div>
-          <span className='rounded-[20px] bg-gray-800 px-[10px] py-[6px] text-body_sm_bold text-white'>
-            1회차 독서
-          </span>
+      <div className='flex w-full flex-col gap-[12px] text-gray-800'>
+        <div
+          onClick={() => router.push('/well-book/2/review/1')}
+          className='flex w-full cursor-pointer flex-col gap-[12px]'
+        >
+          <div>
+            <span className='rounded-[20px] bg-gray-800 px-[10px] py-[6px] text-body_sm_bold text-white'>
+              1회차 독서
+            </span>
+          </div>
+          <Rating rating={4.5} textClass='text-h_lg_bold' />
+          <h3 className='text-title_xl_bold'>
+            예측불허의 긴장된 상태로 성적표를 처음 받아보았을 때를 잊을 수가
+            없다.
+          </h3>
         </div>
-        <Rating rating={4.5} textClass='text-h_lg_bold' />
-        <h3 className='text-title_xl_bold'>
-          예측불허의 긴장된 상태로 성적표를 처음 받아보았을 때를 잊을 수가 없다.
-        </h3>
+
         <div className='flex w-full flex-col gap-[4px]'>
           <TagSlider type='pros' />
           <TagSlider type='cons' />
