2e89daa3db19e931e7b69a3bcca989bd4bb73591
JIMIN1020 <jimins4920@gmail.com>
2024-07-20 18:57:16 +0900

    [Feat/#35]: rating 초기값 설정

--- src/components/Rating/RatingSelector.tsx
+++ src/components/Rating/RatingSelector.tsx
@@ -29,14 +29,14 @@ function RatingSelector({ type, rating, setValue, watch }: Props) {
 
   return (
     <div className='flex w-full flex-col items-center justify-center gap-[8px] text-gray-800'>
-      <h1 className='text-h_xl_bold'>{currentRating?.toFixed(1) || 3.5}</h1>
+      <h1 className='text-h_xl_bold'>{currentRating?.toFixed(1) || '0.0'}</h1>
       <h4 className='text-body_lg'>
         {currentRating ? ratingMessage[currentRating] : '별점을 남겨주세요'}
       </h4>
       <div className='flex gap-[10px]'>
         {Array.from({ length: 5 }, (_, index) => {
           let num;
-          const cur = currentRating || 3.5;
+          const cur = currentRating || 0;
           if (index + 1 <= cur) {
             num = 1;
           } else if (index + 0.5 === cur) {
