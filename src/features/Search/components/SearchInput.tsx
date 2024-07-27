f712b3cc5d8df22f5736f3a34ce25f899861e634
JIMIN1020 <jimins4920@gmail.com>
2024-07-27 17:59:12 +0900

    [Feat/#37]: input clear 버튼 추가

--- src/features/Search/components/SearchInput.tsx
+++ src/features/Search/components/SearchInput.tsx
@@ -1,19 +1,33 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import SearchIcon from 'public/icons/common/navigation/search-icon.svg';
+import ClearIcon from 'public/icons/common/clear.svg';
 
 function SearchInput() {
+  const [searchValue, setSearchValue] = useState('');
+
   return (
     <div className='relative flex w-full'>
-      <input
-        type='text'
-        className='input-common input-light flex-1 pl-[48px] focus:border-main'
-      />
       <SearchIcon
         fill='#8B95A1'
         className='absolute left-[16px] top-1/2 -translate-y-1/2'
       />
+      <input
+        type='text'
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        className='input-common input-light flex-1 px-[48px] focus:border-main'
+      />
+      {searchValue && (
+        <button
+          type='button'
+          onClick={() => setSearchValue('')}
+          className='absolute right-[16px] top-1/2 -translate-y-1/2'
+        >
+          <ClearIcon />
+        </button>
+      )}
     </div>
   );
 }
