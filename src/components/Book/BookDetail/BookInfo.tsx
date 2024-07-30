fa81c1dbe34f2b04b441b07bc84ef3d38078359e
JIMIN1020 <jimins4920@gmail.com>
2024-07-30 14:49:04 +0900

    [Feat/#37]: 도서 정보 컴포넌트

--- src/components/Book/BookDetail/BookInfo.tsx
+++ src/components/Book/BookDetail/BookInfo.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
 function BookInfo() {
-  return <div>BookInfo</div>;
+  return (
+    <div className='flex w-full flex-col'>
+      <div className='flex w-full flex-col gap-[20px] px-page py-[36px]'>
+        <div className='flex w-full flex-col gap-[8px]'>
+          <h5 className='book-info-title'>출판사</h5>
+          <span className='book-info-sub'>행복한 출판사</span>
+        </div>
+        <div className='flex w-full flex-col gap-[8px]'>
+          <h5 className='book-info-title'>ISBN</h5>
+          <span className='book-info-sub'>9791193154250</span>
+        </div>
+      </div>
+      <hr className='h-[2px] w-full bg-gray-300' />
+      <div className='flex w-full flex-col gap-[20px] px-page py-[36px]'>
+        <h5 className='book-info-title'>책 소개</h5>
+        <p className='book-info-sub break-all'>
+          책 소개 입니다. 책 소개 입니다. 책 소개 입니다. 책 소개 입니다. 책
+          소개 입니다. 책 소개 입니다. 책 소개 입니다. 책 소개 입니다. 책 소개
+          입니다. 책 소개 입니다. 책 소개 입니다. 책 소개 입니다. 책 소개
+          입니다. 책 소개 입니다. 책 소개 입니다. 책 소개 입니다. 책 소개
+          입니다. 책 소개 입니다. 책 소개 입니다.{' '}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default BookInfo;
