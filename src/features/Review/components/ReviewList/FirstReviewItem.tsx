f185d6c8b31e2bfca27e5ee3efa1b3220f7bfb82
JIMIN1020 <jimins4920@gmail.com>
2024-07-31 14:19:40 +0900

    [Fix/#37]: 다회독 도전 버튼 제거

--- src/features/Review/components/ReviewList/FirstReviewItem.tsx
+++ src/features/Review/components/ReviewList/FirstReviewItem.tsx
@@ -3,12 +3,8 @@
 import Image from 'next/image';
 import React from 'react';
 import { IMAGES } from '@/constants/images';
-import { usePopUpActions } from '@/store/popUpStore';
-import Button from '@/components/Button/Button';
-import WellSelectSheet from '@/components/PopUp/WellSelectSheet';
 
 function FirstReviewItem() {
-  const { changePopUpState } = usePopUpActions();
   return (
     <div className='review-item items-center text-center'>
       <h4 className='text-title_xl_bold'>
@@ -26,14 +22,6 @@ function FirstReviewItem() {
         <br />
         재미있게 읽었다면 한 번 더 읽어보는 건 어떨까요?
       </span>
-      <Button
-        type='button'
-        onClick={() => changePopUpState('isOpenWellSheet', true)}
-        theme='gray'
-      >
-        다회독 도전하기
-      </Button>
-      <WellSelectSheet />
     </div>
   );
 }
