4d2f4ee80db3f08b43d5594513014b70915a10f3
JIMIN1020 <jimins4920@gmail.com>
2024-08-04 19:25:24 +0900

    [Feat/#37]: 카테고리별 필터링 기능

--- src/features/Search/hooks/useSearch.ts
+++ src/features/Search/hooks/useSearch.ts
@@ -3,12 +3,18 @@ import { useSearchParams } from 'next/navigation';
 import { searchBook } from '../api/search.api';
 
 export const useSearch = () => {
-  const searchValue = useSearchParams().get('query');
+  const queries = useSearchParams();
+  const searchValue = queries.get('query');
+  const category = queries.get('category');
 
   const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery({
-    queryKey: ['search', searchValue],
+    queryKey: ['search', [searchValue, category]],
     queryFn: ({ pageParam }) =>
-      searchBook({ q: searchValue!, page: pageParam }),
+      searchBook({
+        q: searchValue!,
+        page: pageParam,
+        category: category || undefined,
+      }),
     initialPageParam: 0,
     getNextPageParam: (lastPage) => {
       const isLastPage =
