17dbfa44131609d5874b2bb4596c4f90aa364ac3
JIMIN1020 <jimins4920@gmail.com>
2024-08-13 16:09:21 +0900

    [Feat/#39]: 태그 슬라이더 구현

--- src/features/Memo/components/Tag/MemoTagList.tsx
+++ src/features/Memo/components/Tag/MemoTagList.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useRef, useState } from 'react';
+import { motion } from 'framer-motion';
 import MemoTag from './MemoTag';
 
 function MemoTagList() {
+  const sliderRef = useRef<HTMLDivElement | null>(null);
+  const motionDivRef = useRef<HTMLDivElement | null>(null);
+  const [drag, setDrag] = useState<'x' | undefined>(undefined);
+
+  useEffect(() => {
+    if (
+      sliderRef.current &&
+      motionDivRef.current &&
+      sliderRef.current.offsetWidth - 48 >= motionDivRef.current.offsetWidth
+    ) {
+      setDrag(undefined);
+    } else {
+      setDrag('x');
+    }
+  }, []);
+
   return (
-    <div className='flex w-full gap-[8px]'>
-      <MemoTag type='phrase' />
-      <MemoTag type='thought' />
-      <MemoTag type='question' />
+    <div ref={sliderRef} className='relative flex w-full overflow-auto'>
+      <motion.div
+        ref={motionDivRef}
+        drag={drag}
+        dragConstraints={sliderRef}
+        dragElastic={0.2}
+        className='flex w-fit gap-[8px] px-[24px]'
+      >
+        <MemoTag type='phrase' />
+        <MemoTag type='thought' />
+        <MemoTag type='question' />
+        <MemoTag type='question' />
+        <MemoTag type='question' />
+      </motion.div>
     </div>
   );
 }
