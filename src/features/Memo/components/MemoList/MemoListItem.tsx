39a104e7a675ca15af822c6c814a41cd895f161b
JIMIN1020 <jimins4920@gmail.com>
2024-08-14 21:07:07 +0900

    [Style/#39]: 페이지, 태그 제거

--- src/features/Memo/components/MemoList/MemoListItem.tsx
+++ src/features/Memo/components/MemoList/MemoListItem.tsx
@@ -3,39 +3,29 @@
 import DeleteButton from '@/components/ListItem/DeleteButton';
 import { useRouter } from 'next/navigation';
 import React from 'react';
-import PageTag from '../Tag/PageTag';
-import MemoTagList from '../Tag/MemoTagList';
 
 function MemoListItem() {
   const router = useRouter();
 
   return (
     <div className='review-item px-0 pb-0'>
-      <div className='flex w-full cursor-pointer flex-col gap-[20px]'>
-        <div className='flex w-full flex-col gap-[8px]'>
-          <div className='px-page'>
-            <PageTag pageNum={100} />
-          </div>
-          <MemoTagList />
+      <div
+        className='flex w-full flex-col gap-[20px] px-page'
+        onClick={() => router.push(`/well-book/9791193154250/memo/1`)}
+      >
+        <div className='flex w-full gap-[20px]'>
+          <div className='h-[160px] w-[160px] rounded-[8px] bg-gray-100' />
+          <div className='h-[160px] w-[160px] rounded-[8px] bg-gray-100' />
         </div>
-        <div
-          className='flex w-full flex-col gap-[20px] px-page'
-          onClick={() => router.push(`/well-book/9791193154250/memo/1`)}
-        >
-          <div className='flex w-full gap-[20px]'>
-            <div className='h-[160px] w-[160px] rounded-[8px] bg-gray-100' />
-            <div className='h-[160px] w-[160px] rounded-[8px] bg-gray-100' />
-          </div>
-          <p>
-            내가 책을 덮으면서 느낀 감정을 한마디로 표현을 내가 책을 덮으면서
-            느낀 감정을 한마디로 표현...
-          </p>
-          <div className='flex w-full justify-between'>
-            <span className='text-body_md text-gray-600'>
-              2023.07.03 (수정됨)
-            </span>
-            <span className='text-body_lg_bold text-main'>공개</span>
-          </div>
+        <p>
+          내가 책을 덮으면서 느낀 감정을 한마디로 표현을 내가 책을 덮으면서 느낀
+          감정을 한마디로 표현...
+        </p>
+        <div className='flex w-full justify-between'>
+          <span className='text-body_md text-gray-600'>
+            2023.07.03 (수정됨)
+          </span>
+          <span className='text-body_lg_bold text-main'>공개</span>
         </div>
       </div>
       <DeleteButton buttonText='메모 삭제' />
