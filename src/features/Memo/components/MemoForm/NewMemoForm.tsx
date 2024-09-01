9a6cd8225ae3548f15afedd226c17e5a226066da
JIMIN1020 <jimins4920@gmail.com>
2024-09-02 00:34:03 +0900

    [API/#45]: 메모 작성 API 연동

--- src/features/Memo/components/MemoForm/NewMemoForm.tsx
+++ src/features/Memo/components/MemoForm/NewMemoForm.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import React from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { FormProvider, useForm } from 'react-hook-form';
 import { MemoFormType } from '../../types/form';
 import MemoForm from './MemoForm';
+import { addNewMemo } from '../../api/memo.api';
 
 function NewMemoForm() {
+  const router = useRouter();
+  const id = useSearchParams().get('id');
+  const { data: session } = useSession();
   const methods = useForm<MemoFormType>({
     mode: 'onBlur',
     defaultValues: {
@@ -15,14 +21,26 @@ function NewMemoForm() {
     },
   });
 
-  const handleAddTodo = () => {
-    //
+  const handleAddMemo = (data: MemoFormType) => {
+    if (!session || !id) return;
+
+    addNewMemo({
+      writer: session.user.id,
+      isbn: id,
+      content: data.memo,
+      is_public: data.isPublic,
+      images: data.images,
+    }).then((res) => {
+      if (res?.result) {
+        router.push(`/well-book/${res.id}/memo`);
+      }
+    });
   };
 
   return (
     <FormProvider {...methods}>
       <form
-        onSubmit={methods.handleSubmit(handleAddTodo)}
+        onSubmit={methods.handleSubmit(handleAddMemo)}
         className='flex h-dvh w-full flex-1 flex-col bg-white'
       >
         <MemoForm />
