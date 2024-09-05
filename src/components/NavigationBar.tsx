53c7eb80c3a75717bbce1a2f39bdc1af8005fc6d
JIMIN1020 <jimins4920@gmail.com>
2024-09-05 20:52:35 +0900

    [Style/#45]: nav bar 모션 추가

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -4,20 +4,24 @@ import Link from 'next/link';
 import React from 'react';
 import { usePathname } from 'next/navigation';
 import { FeedIcon, ProfileIcon, SearchIcon, WellIcon } from 'public/icons';
+import { motion } from 'framer-motion';
+
+const MotionLink = motion(Link);
 
 function NavigationBar() {
   const pathname = usePathname();
+
   return (
     <div className='flex h-[80px] w-full shrink-0 items-center justify-around bg-white px-[20px]'>
-      <Link href='/' className='navItem'>
+      <MotionLink whileTap={{ scale: 1.2 }} href='/' className='navItem'>
         <WellIcon fill={pathname === '/' ? '#313239' : '#B3B6C5'} height={22} />
         <span
           className={`text-body-sm-bold ${pathname === '/' ? 'text-gray-800' : 'text-gray-500'}`}
         >
           우물
         </span>
-      </Link>
-      <Link href='/feed' className='navItem'>
+      </MotionLink>
+      <MotionLink whileTap={{ scale: 1.2 }} href='/feed' className='navItem'>
         <FeedIcon
           fill={pathname === '/feed' ? '#313239' : '#B3B6C5'}
           height={22}
@@ -27,8 +31,12 @@ function NavigationBar() {
         >
           피드
         </span>
-      </Link>
-      <Link href='/search-main' className='navItem'>
+      </MotionLink>
+      <MotionLink
+        whileTap={{ scale: 1.2 }}
+        href='/search-main'
+        className='navItem'
+      >
         <SearchIcon
           fill={pathname === '/search-main' ? '#313239' : '#B3B6C5'}
         />
@@ -37,15 +45,15 @@ function NavigationBar() {
         >
           책 검색
         </span>
-      </Link>
-      <Link href='/profile' className='navItem'>
+      </MotionLink>
+      <MotionLink whileTap={{ scale: 1.2 }} href='/profile' className='navItem'>
         <ProfileIcon fill={pathname === '/profile' ? '#313239' : '#B3B6C5'} />
         <span
           className={`text-body-sm-bold ${pathname === '/profile' ? 'text-gray-800' : 'text-gray-500'}`}
         >
           내 정보
         </span>
-      </Link>
+      </MotionLink>
     </div>
   );
 }
