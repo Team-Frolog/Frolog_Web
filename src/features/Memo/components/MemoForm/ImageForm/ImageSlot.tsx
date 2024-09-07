34cfdd0cd89106205a5a0cd2a4e4ba1a52aa146e
JIMIN1020 <jimins4920@gmail.com>
2024-09-07 22:12:54 +0900

    [Fix/#45]: 메모 이미지 업로드 시 깜빡임 제거

--- src/features/Memo/components/MemoForm/ImageForm/ImageSlot.tsx
+++ src/features/Memo/components/MemoForm/ImageForm/ImageSlot.tsx
@@ -23,17 +23,12 @@ function ImageSlot({
 
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth >= 450) {
-        setDimensions({
-          width: 450 - 48,
-          height: 450 - 48,
-        });
-      } else {
-        setDimensions({
-          width: window.innerWidth - 48,
-          height: window.innerWidth - 48,
-        });
-      }
+      const newSize =
+        window.innerWidth >= 450 ? 450 - 48 : window.innerWidth - 48;
+      setDimensions({
+        width: newSize,
+        height: newSize,
+      });
     };
 
     handleResize();
@@ -49,44 +44,47 @@ function ImageSlot({
         height: `${dimensions.height}px`,
       }}
     >
-      {src ? (
-        <>
-          <Image
-            src={src}
-            alt={`memo-img-${index}`}
-            width={290}
-            height={290}
-            className='h-full w-full object-cover'
-            onClick={
-              isReadOnly
-                ? undefined
-                : (e) => {
-                    e.stopPropagation();
-                    setIsPreviewOpen(true);
-                  }
-            }
-          />
-          {!isReadOnly && (
-            <button
-              type='button'
-              onClick={onDelete}
-              className='absolute right-[8px] top-[8px] cursor-pointer'
-            >
-              <DeleteImgIcon />
-            </button>
-          )}
-        </>
-      ) : (
-        <div className='relative flex h-full w-full items-center justify-center rounded-[8px] bg-gray-200'>
-          <input
-            type='file'
-            accept='image/*'
-            onChange={onChange}
-            className='absolute left-0 top-0 h-full w-full cursor-pointer opacity-0'
-          />
-          <ImgPlusIcon />
-        </div>
-      )}
+      <div className='relative flex h-full w-full items-center justify-center rounded-[8px] bg-gray-200'>
+        {src ? (
+          <>
+            <Image
+              src={src}
+              alt={`memo-img-${index}`}
+              width={290}
+              height={290}
+              className='h-full w-full object-cover'
+              priority
+              onClick={
+                isReadOnly
+                  ? undefined
+                  : (e) => {
+                      e.stopPropagation();
+                      setIsPreviewOpen(true);
+                    }
+              }
+            />
+            {!isReadOnly && (
+              <button
+                type='button'
+                onClick={onDelete}
+                className='absolute right-[8px] top-[8px] cursor-pointer'
+              >
+                <DeleteImgIcon />
+              </button>
+            )}
+          </>
+        ) : (
+          <>
+            <input
+              type='file'
+              accept='image/*'
+              onChange={onChange}
+              className='absolute left-0 top-0 h-full w-full cursor-pointer opacity-0'
+            />
+            <ImgPlusIcon />
+          </>
+        )}
+      </div>
       {isPreviewOpen && (
         <ImagePreview
           imgSrc={src!}
