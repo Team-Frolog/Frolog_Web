f26598cb6ca7277ac812d730fcb0a6a882dc9526
JIMIN1020 <jimins4920@gmail.com>
2024-09-10 21:08:15 +0900

    [Style/#46]: 피드 댓글 페이지 레이아웃

---
+++ src/app/(default)/feed/[id]/comments/page.tsx
@@ -0,0 +1,16 @@
+import BackHeader from '@/components/Header/BackHeader';
+import MainLayout from '@/layouts/MainLayout';
+import React from 'react';
+
+function CommentPage() {
+  return (
+    <div className='flex w-full flex-1 flex-col'>
+      <BackHeader />
+      <MainLayout>
+        <div>dd</div>
+      </MainLayout>
+    </div>
+  );
+}
+
+export default CommentPage;
