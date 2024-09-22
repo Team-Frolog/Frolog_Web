e317c67058c23ecc84408397d18faf5d7c8798c2
JIMIN1020 <jimins4920@gmail.com>
2024-09-22 13:45:05 +0900

    [Fix/#51]: 빌드 오류, 일부 디자인 수정

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -12,7 +12,7 @@ function NavigationBar() {
   const pathname = usePathname();
 
   return (
-    <div className='flex h-[80px] w-full shrink-0 items-center justify-around bg-white px-[20px]'>
+    <div className='flex h-[80px] w-full shrink-0 items-center justify-around border-t border-gray-300 bg-white px-[20px]'>
       <MotionLink whileTap={{ scale: 1.2 }} href='/' className='navItem'>
         <WellIcon fill={pathname === '/' ? '#313239' : '#B3B6C5'} height={22} />
         <span
