b013b63517858d31fad422886624c1a2e90ebc41
JIMIN1020 <jimins4920@gmail.com>
2024-09-26 14:51:07 +0900

    [Feat/#53]: 읽는 중인 책 구현

--- src/features/Well/components/WellBook/WellReadingBook.tsx
+++ src/features/Well/components/WellBook/WellReadingBook.tsx
@@ -8,10 +8,9 @@ import { useSession } from 'next-auth/react';
 
 interface Props {
   userId?: string;
-  isReading?: boolean;
 }
 
-function WellBook({ userId, isReading = false }: Props) {
+function WellReadingBook({ userId }: Props) {
   const router = useRouter();
   const { data: session } = useSession();
   const newReviewId = useNewReviewId();
@@ -26,23 +25,14 @@ function WellBook({ userId, isReading = false }: Props) {
       variants={newReviewId ? staggerItemVariants : undefined} // 이 책의 id와 같으면 스택 실행
       className='flex w-[85%] items-center'
     >
-      {isReading ? (
-        <div className='relative flex flex-1 justify-center rounded-l-[100px] border-[8px] border-r-0 border-category-bg-religion bg-white px-[24px] py-[5px]'>
-          <span className='text-overflow-hidden w-[82%] text-center text-body-sm-bold text-gray-700'>
-            메리와 메리
-          </span>
-        </div>
-      ) : (
-        <div className='relative flex flex-1 justify-center bg-category-bg-novel px-[18px] py-[12px]'>
-          <div className='absolute left-[20px] top-0 h-full w-[12px] bg-category-band-novel' />
-          <span className='text-overflow-hidden w-[82%] text-center text-body-sm-bold'>
-            메리와 메리
-          </span>
-        </div>
-      )}
+      <div className='relative flex flex-1 justify-center rounded-l-[100px] border-[8px] border-r-0 border-category-bg-religion bg-white px-[24px] py-[5px]'>
+        <span className='text-overflow-hidden w-[82%] text-center text-body-sm-bold text-gray-700'>
+          메리와 메리
+        </span>
+      </div>
       <MemoBookmarkIcon />
     </motion.div>
   );
 }
 
-export default WellBook;
+export default WellReadingBook;
