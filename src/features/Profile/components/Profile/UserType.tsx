01e306d066fa47e0383070bf7fb07a3de5530a1c
JIMIN1020 <jimins4920@gmail.com>
2024-09-30 15:08:45 +0900

    [Style/#54]: 프로필 개요 -> 비공개인 경우

--- src/features/Profile/components/Profile/UserType.tsx
+++ src/features/Profile/components/Profile/UserType.tsx
@@ -9,27 +9,19 @@ function UserType() {
       slideClass='gap-[8px]'
     >
       <div className='flex flex-col items-center gap-[4px]'>
-        <div className='w-fit whitespace-nowrap rounded-[12px] bg-gray-200 px-[22px] py-[8px] text-body-lg-bold text-gray-800'>
-          감정형
-        </div>
+        <div className='user-type-chip'>감정형</div>
         <span className='text-body-sm text-gray-600'>독서성향</span>
       </div>
       <div className='flex flex-col items-center gap-[4px]'>
-        <div className='w-fit whitespace-nowrap rounded-[12px] bg-gray-200 px-[22px] py-[8px] text-body-lg-bold text-gray-800'>
-          20대
-        </div>
+        <div className='user-type-chip-disabled'>비공개</div>
         <span className='text-body-sm text-gray-600'>연령대</span>
       </div>
       <div className='flex flex-col items-center gap-[4px]'>
-        <div className='w-fit whitespace-nowrap rounded-[12px] bg-gray-200 px-[22px] py-[8px] text-body-lg-bold text-gray-800'>
-          남자
-        </div>
+        <div className='user-type-chip'>남자</div>
         <span className='text-body-sm text-gray-600'>성별</span>
       </div>
       <div className='flex flex-col items-center gap-[4px]'>
-        <div className='w-fit whitespace-nowrap rounded-[12px] bg-gray-200 px-[22px] py-[8px] text-body-lg-bold text-gray-800'>
-          디자인/크리에이티브
-        </div>
+        <div className='user-type-chip'>디자인/크리에이티브</div>
         <span className='text-body-sm text-gray-600'>직업</span>
       </div>
     </Slider>
