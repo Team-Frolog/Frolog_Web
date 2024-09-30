2dda4914ca57c82419cd84b6b4fc2bea988c43ed
JIMIN1020 <jimins4920@gmail.com>
2024-09-30 15:04:29 +0900

    [Style/#54]: 프로필 메인 UI 수정

--- src/features/Profile/components/Profile/Profile.tsx
+++ src/features/Profile/components/Profile/Profile.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import LinkButton from '@/components/Button/LinkButton';
 import UserStatistics from './UserStatistics';
 import UserType from './UserType';
-import IntroInput from './IntroInput';
 
 interface Props {
   userId?: string;
@@ -15,14 +14,6 @@ function Profile({ userId }: Props) {
     <div className='flex w-full flex-col gap-[28px]'>
       <UserStatistics />
       <UserType />
-      {userId ? (
-        <div className='flex w-full flex-col gap-[8px] px-page'>
-          <h6 className='mb-[4px] text-body-md text-gray-700'>자기소개</h6>
-          <div className='input-common input-light w-full'>자기소개</div>
-        </div>
-      ) : (
-        <IntroInput />
-      )}
       {!userId && (
         <div className='flex px-page'>
           <LinkButton route='/profile/edit' theme='gray'>
