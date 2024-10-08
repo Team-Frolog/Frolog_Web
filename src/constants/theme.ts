5d90fa66b04de3c9ea2989362457038f04e3fd98
JIMIN1020 <jimins4920@gmail.com>
2024-10-08 11:44:28 +0900

    [Style/#48]: gray theme 적용

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,5 +1,6 @@
 const DARK = 'dark';
 const LIGHT = 'light';
+const GRAY = 'gray';
 const HALF = 'half-gradient';
 
 export const PAGE_THEME: {
@@ -12,14 +13,14 @@ export const PAGE_THEME: {
   '/login': DARK,
   '/find-password': DARK,
   '/frolog-test': LIGHT,
-  '/frolog-test/result/1': HALF,
-  '/frolog-test/result/2': HALF,
-  '/frolog-test/result/3': HALF,
+  '/frolog-test/result/1': DARK,
+  '/frolog-test/result/2': DARK,
+  '/frolog-test/result/3': DARK,
   '/well': HALF,
   '/writing': HALF,
   '/new-review': HALF,
   '/search': LIGHT,
-  '/feed': LIGHT,
+  '/feed': GRAY,
   '/profile': LIGHT,
   '/well-book': HALF,
 };
