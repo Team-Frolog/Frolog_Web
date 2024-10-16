3d4694112bec3e732f524d506a8ea3a14acd70b6
JIMIN1020 <jimins4920@gmail.com>
2024-10-17 00:18:23 +0900

    [Feat/#60]: 개구리 3개 이하인 경우 > expansion 제한

--- src/features/Well/components/WellForm/Frog/FrogSelector.tsx
+++ src/features/Well/components/WellForm/Frog/FrogSelector.tsx
@@ -11,13 +11,15 @@ function FrogSelector() {
   const controls = useAnimationControls();
   const [isExpanded, setIsExpanded] = useState(false);
 
+  const isNoExpansion = frogs ? frogs.length < 3 : true;
+
   useEffect(() => {
-    if (isExpanded) {
+    if (isExpanded || isNoExpansion) {
       controls.start({ height: 'auto' });
     } else {
       controls.start({ height: '280px' });
     }
-  }, [isExpanded, controls]);
+  }, [isExpanded, controls, isNoExpansion]);
 
   if (!frogs) return <></>;
 
@@ -25,19 +27,21 @@ function FrogSelector() {
     <div className='flex w-full flex-col gap-[12px]'>
       <h6 className='text-body-md text-gray-700'>내 캐릭터</h6>
       <motion.div
-        initial={{ height: isExpanded || frogs.length > 3 ? 'auto' : '280px' }}
+        initial={{ height: isExpanded || isNoExpansion ? 'auto' : '280px' }}
         animate={controls}
         transition={{ duration: 0.3 }}
         className='relative flex flex-wrap gap-[9px] overflow-hidden'
       >
-        <div
-          className={`absolute bottom-0 left-0 h-[90px] w-full transition-all duration-300 ${isExpanded ? '-z-10 opacity-100' : 'z-10 opacity-100'}`}
-          style={{
-            background:
-              'linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, #FFF 95.41%)',
-          }}
-        />
-        {frogs?.map((frog) => (
+        {!isNoExpansion && (
+          <div
+            className={`absolute bottom-0 left-0 h-[90px] w-full transition-all duration-300 ${isExpanded ? '-z-10 opacity-100' : 'z-10 opacity-100'}`}
+            style={{
+              background:
+                'linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, #FFF 95.41%)',
+            }}
+          />
+        )}
+        {frogs.map((frog) => (
           <FrogCharacter
             key={frog.id}
             data={frog}
@@ -46,7 +50,7 @@ function FrogSelector() {
           />
         ))}
       </motion.div>
-      {frogs.length > 3 && (
+      {!isNoExpansion && (
         <button
           type='button'
           className='flex w-full justify-center bg-white'
--- src/features/Well/components/WellForm/WellForm.tsx
+++ src/features/Well/components/WellForm/WellForm.tsx
@@ -2,27 +2,27 @@
 
 import TitleHeader from '@/components/Header/TitleHeader';
 import React from 'react';
-import GenericForm from '@/components/Form/GenericForm';
-import { useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import WellNameInput from './WellNameInput';
 import FrogSelector from './Frog/FrogSelector';
 import ShapeForm from './Shape/ShapeForm';
 
 export interface WellFormType {
   name: string;
-  frog: number | null;
-  color: string | null;
-  shape: number | null;
+  frog: string;
+  color: string;
+  shape: number;
 }
 
 function WellForm() {
   const methods = useForm<WellFormType>({
     mode: 'onChange',
-    defaultValues: { name: '', frog: 1, color: 'novel', shape: 1 },
+    defaultValues: { name: '', frog: 'default', color: 'novel', shape: 1 },
   });
 
   const {
     watch,
+    handleSubmit,
     formState: { errors },
   } = methods;
 
@@ -31,26 +31,24 @@ function WellForm() {
   };
 
   return (
-    <GenericForm<WellFormType>
-      onSubmit={handleAddNewWell}
-      formOptions={{
-        mode: 'onChange',
-        defaultValues: { name: '', frog: 1, color: 'novel', shape: 1 },
-      }}
-      className='form-layout gap-0'
-    >
-      <TitleHeader
-        title='새 우물 파기'
-        theme='light'
-        type='write'
-        isDisabled={!watch('name') || !!errors.name}
-      />
-      <div className='flex w-full flex-1 flex-col gap-[36px] overflow-auto bg-white px-page py-[32px]'>
-        <WellNameInput />
-        <FrogSelector />
-        <ShapeForm />
-      </div>
-    </GenericForm>
+    <FormProvider {...methods}>
+      <form
+        className='form-layout gap-0'
+        onSubmit={handleSubmit(handleAddNewWell)}
+      >
+        <TitleHeader
+          title='새 우물 파기'
+          theme='light'
+          type='write'
+          isDisabled={!watch('name') || !!errors.name}
+        />
+        <div className='flex w-full flex-1 flex-col gap-[36px] overflow-auto bg-white px-page py-[32px]'>
+          <WellNameInput />
+          <FrogSelector />
+          <ShapeForm />
+        </div>
+      </form>
+    </FormProvider>
   );
 }
 
