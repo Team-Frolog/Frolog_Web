d3793b1c7e42fd3c9e9a6b8b6f3302f4172864c5
JIMIN1020 <jimins4920@gmail.com>
2024-10-17 15:31:10 +0900

    [Comment/#60]: 우물 관련 임시 주석처리

--- src/components/PopUp/WellSelectSheet.tsx
+++ src/components/PopUp/WellSelectSheet.tsx
@@ -1,19 +1,18 @@
-import { usePopUpActions, useWellSheetState } from '@/store/popUpStore';
+import { useWellSheetState } from '@/store/popUpStore';
 import { AnimatePresence } from 'framer-motion';
 import React from 'react';
-import { wellList } from '@/data/dummy/well';
 import { sheetData } from '@/data/ui/bottomSheet';
-import { Well } from '@/features/Well';
+// import { Well } from '@/features/Well';
 import AlertBottomSheet from '../../layouts/AlertBottomSheet';
 
 function WellSelectSheet() {
   const isOpenWellSheet = useWellSheetState();
-  const { changePopUpState } = usePopUpActions();
+  // const { changePopUpState } = usePopUpActions();
 
-  const handleSelectWell = () => {
-    changePopUpState('isOpenWellSheet', false);
-    changePopUpState('isOpenSelectBooksSheet', true);
-  };
+  // const handleSelectWell = () => {
+  //   changePopUpState('isOpenWellSheet', false);
+  //   changePopUpState('isOpenSelectBooksSheet', true);
+  // };
 
   return (
     <AnimatePresence>
@@ -23,14 +22,14 @@ function WellSelectSheet() {
             {sheetData.add_another_to_well.description!()}
           </p>
           <div className='grid max-h-[400px] w-full grid-cols-2 justify-center justify-items-center gap-[20px] overflow-y-auto py-[20px] pb-[40px] scrollbar-hide'>
-            {wellList.map((well) => (
+            {/* {wellList.map((well) => (
               <Well
                 type='select'
                 key={well.id}
                 wellData={well}
                 onClick={handleSelectWell}
               />
-            ))}
+            ))} */}
           </div>
         </AlertBottomSheet>
       )}
