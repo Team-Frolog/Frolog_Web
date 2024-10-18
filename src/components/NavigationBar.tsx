23d8b2abd43f3d3035f8c8b3edb6cc0ae258f4c8
JIMIN1020 <jimins4920@gmail.com>
2024-10-18 20:27:58 +0900

    [Refactor/#60]: 경로 상수화

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -6,6 +6,7 @@ import { usePathname } from 'next/navigation';
 import { FeedIcon, ProfileIcon, SearchIcon, WellIcon } from 'public/icons';
 import { motion } from 'framer-motion';
 import { useSession } from 'next-auth/react';
+import { PAGES } from '@/constants/page';
 
 const MotionLink = motion(Link);
 
@@ -15,42 +16,50 @@ function NavigationBar() {
 
   return (
     <div className='z-50 flex h-[80px] w-full shrink-0 items-center justify-around border-t border-gray-300 bg-white px-[20px]'>
-      <MotionLink whileTap={{ scale: 1.2 }} href='/' className='navItem'>
+      <MotionLink
+        whileTap={{ scale: 1.2 }}
+        href={PAGES.HOME}
+        className='navItem'
+      >
         <WellIcon
           fill={
-            pathname === '/' || pathname.includes('/well')
+            pathname === PAGES.HOME || pathname.includes(PAGES.WELL)
               ? '#313239'
               : '#B3B6C5'
           }
           height={22}
         />
         <span
-          className={`text-body-sm-bold ${pathname === '/' || pathname.includes('/well') ? 'text-gray-800' : 'text-gray-500'}`}
+          className={`text-body-sm-bold ${pathname === PAGES.HOME || pathname.includes(PAGES.WELL) ? 'text-gray-800' : 'text-gray-500'}`}
         >
           우물
         </span>
       </MotionLink>
-      <MotionLink whileTap={{ scale: 1.2 }} href='/feed' className='navItem'>
+      <MotionLink
+        whileTap={{ scale: 1.2 }}
+        href={PAGES.FEED}
+        className='navItem'
+      >
         <FeedIcon
-          fill={pathname === '/feed' ? '#313239' : '#B3B6C5'}
+          fill={pathname === PAGES.FEED ? '#313239' : '#B3B6C5'}
           height={22}
         />
         <span
-          className={`text-body-sm-bold ${pathname === '/feed' ? 'text-gray-800' : 'text-gray-500'}`}
+          className={`text-body-sm-bold ${pathname === PAGES.FEED ? 'text-gray-800' : 'text-gray-500'}`}
         >
           피드
         </span>
       </MotionLink>
       <MotionLink
         whileTap={{ scale: 1.2 }}
-        href='/search-main'
+        href={PAGES.SEARCH_HOME}
         className='navItem'
       >
         <SearchIcon
-          fill={pathname === '/search-main' ? '#313239' : '#B3B6C5'}
+          fill={pathname === PAGES.SEARCH_HOME ? '#313239' : '#B3B6C5'}
         />
         <span
-          className={`text-body-sm-bold ${pathname === '/search-main' ? 'text-gray-800' : 'text-gray-500'}`}
+          className={`text-body-sm-bold ${pathname === PAGES.SEARCH_HOME ? 'text-gray-800' : 'text-gray-500'}`}
         >
           책검색
         </span>
@@ -61,10 +70,10 @@ function NavigationBar() {
         className='navItem'
       >
         <ProfileIcon
-          fill={pathname.includes('profile') ? '#313239' : '#B3B6C5'}
+          fill={pathname.includes(PAGES.PROFILE) ? '#313239' : '#B3B6C5'}
         />
         <span
-          className={`text-body-sm-bold ${pathname.includes('profile') ? 'text-gray-800' : 'text-gray-500'}`}
+          className={`text-body-sm-bold ${pathname.includes(PAGES.PROFILE) ? 'text-gray-800' : 'text-gray-500'}`}
         >
           프로필
         </span>
