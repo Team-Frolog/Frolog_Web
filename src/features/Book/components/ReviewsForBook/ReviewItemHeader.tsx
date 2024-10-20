4d6787e86cc27722c1760da7f93915adaf705ea1
JIMIN1020 <jimins4920@gmail.com>
2024-10-20 19:04:11 +0900

    [Style/#62]: 리뷰 모음 헤더 -> 카테고리 컬러 적용

--- src/features/Book/components/ReviewsForBook/ReviewItemHeader.tsx
+++ src/features/Book/components/ReviewsForBook/ReviewItemHeader.tsx
@@ -9,10 +9,12 @@ interface Props {
 function ReviewItemHeader({ rating, category }: Props) {
   return (
     <div className='pt-[30px]'>
-      <div className='tooltip-feed relative flex w-full gap-[16px] rounded-t-[20px] bg-category-bg-novel px-page py-[12px] after:border-b-category-bg-novel'>
+      <div
+        className={`tooltip-feed relative flex w-full gap-[16px] rounded-t-[20px] bg-category-bg-${category} px-page py-[12px] after:border-b-category-bg-${category}`}
+      >
         <Rating
           rating={rating}
-          textClass='text-heading-lg-bold text-category-text-novel'
+          textClass={`text-heading-lg-bold text-category-text-${category}`}
           category={category}
           hasComment
         />
