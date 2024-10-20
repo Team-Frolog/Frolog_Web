0131719508e4af43e10602a0e63e74351344dbbb
JIMIN1020 <jimins4920@gmail.com>
2024-10-20 13:49:39 +0900

    [Fix/#62]: 우물 리스트 스켈레톤 수정

--- src/components/Fallback/Skeleton/WellListSkeleton.tsx
+++ src/components/Fallback/Skeleton/WellListSkeleton.tsx
@@ -5,7 +5,10 @@ function WellListSkeleton() {
   return (
     <div className='relative flex w-full flex-col bg-gray-300 pb-[30px]'>
       <div className='grid grid-cols-2 gap-[24px] px-page py-[12px]'>
+        <div className='flex h-fit w-fit flex-col items-center gap-[8px] place-self-center'>
         <WellAddIcon className='h-[161px] w-[161px]' />
+        <span className='text-body-lg-bold text-gray-800'>새 우물 파기</span>
+        </div>
         <div className='flex h-fit w-fit flex-col items-center gap-[8px] place-self-center'>
           <div className='flex-center skeleton-box skeleton-animation h-[160px] w-[160px] rounded-[50%]' />
           <p className='skeleton-text w-[70px]' />
