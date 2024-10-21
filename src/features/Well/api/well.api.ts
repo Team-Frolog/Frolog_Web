21425e8db35ec43854002a02929bb1c95e67c150
JIMIN1020 <jimins4920@gmail.com>
2024-10-21 18:25:15 +0900

    [Fix/#62]: 우물 아이템 정렬 수정

--- src/features/Well/api/well.api.ts
+++ src/features/Well/api/well.api.ts
@@ -58,7 +58,7 @@ export const getWell = async (id: string) => {
 
 export const getWellItems = async (well_id: string) => {
   try {
-    const response = await searchWellItem.fetch({ well_id });
+    const response = await searchWellItem.fetch({ well_id, sort: 'oldest' });
     return response;
   } catch (err) {
     toast.error(ERROR_ALERT);
