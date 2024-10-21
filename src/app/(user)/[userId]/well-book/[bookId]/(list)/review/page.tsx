83d82430034b7051e1a7d58873793b0e7f2c5c8b
JIMIN1020 <jimins4920@gmail.com>
2024-10-21 13:58:39 +0900

    [Feat/#62]: 리뷰 하나 이상일 경우 버튼 제거

--- src/app/(user)/[userId]/well-book/[bookId]/(list)/review/page.tsx
+++ src/app/(user)/[userId]/well-book/[bookId]/(list)/review/page.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import AddButton from '@/components/Button/AddButton';
 import ReviewListSkeleton from '@/components/Fallback/Skeleton/ReviewListSkeleton';
 import dynamic from 'next/dynamic';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/utils/auth/auth';
+import { getIsRootUser } from '@/utils/auth/getIsRootUser';
 
 const ReviewList = dynamic(
   () => import('@/features/Review/components/ReviewList/ReviewList'),
@@ -21,18 +19,9 @@ interface Props {
 }
 
 async function ReviewPage({ params: { userId, bookId } }: Props) {
-  const session = await getServerSession(authOptions);
+  const { isRootUser } = await getIsRootUser(userId);
 
-  return (
-    <>
-      {userId === session?.user.id && (
-        <div className='add-button-wrapper'>
-          <AddButton route={`/new-review?id=${bookId}`} text='리뷰 추가하기' />
-        </div>
-      )}
-      <ReviewList bookId={bookId} userId={userId} />
-    </>
-  );
+  return <ReviewList bookId={bookId} userId={userId} isRootUser={isRootUser} />;
 }
 
 export default ReviewPage;
--- src/features/Review/components/ReviewList/ReviewList.tsx
+++ src/features/Review/components/ReviewList/ReviewList.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import EmptyContentFrog from '@/components/Fallback/EmptyContentFrog';
+import AddButton from '@/components/Button/AddButton';
 import ReviewListItem from './ReviewListItem';
 import FirstReviewItem from './FirstReviewItem';
 import { useReviews } from '../../hooks/useReviews';
@@ -9,32 +10,40 @@ import { useReviews } from '../../hooks/useReviews';
 interface Props {
   bookId: string;
   userId: string;
+  isRootUser: boolean;
 }
 
-function ReviewList({ bookId, userId }: Props) {
+function ReviewList({ bookId, userId, isRootUser }: Props) {
   const { reviews, setReviewId, deleteReview, isEmpty, isFetched } = useReviews(
     bookId,
     userId
   );
 
   return (
-    <div className='z-10 flex w-full flex-1 flex-col gap-[12px]'>
-      {isEmpty && isFetched && (
-        <EmptyContentFrog title='책을 다 읽으셨으면 이제 리뷰를 써보세요!' />
+    <>
+      {isRootUser && isEmpty && (
+        <div className='add-button-wrapper'>
+          <AddButton route={`/new-review?id=${bookId}`} text='리뷰 추가하기' />
+        </div>
       )}
-      {!isEmpty && isFetched && (
-        <>
-          <ReviewListItem
-            key={reviews[0].id}
-            reviewData={reviews[0]}
-            onDelete={deleteReview}
-            setReviewId={setReviewId}
-            userId={userId}
-          />
-          <FirstReviewItem />
-        </>
-      )}
-    </div>
+      <div className='z-10 flex w-full flex-1 flex-col gap-[12px]'>
+        {isEmpty && isFetched && (
+          <EmptyContentFrog title='책을 다 읽으셨으면 이제 리뷰를 써보세요!' />
+        )}
+        {!isEmpty && isFetched && (
+          <>
+            <ReviewListItem
+              key={reviews[0].id}
+              reviewData={reviews[0]}
+              onDelete={deleteReview}
+              setReviewId={setReviewId}
+              userId={userId}
+            />
+            <FirstReviewItem />
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
