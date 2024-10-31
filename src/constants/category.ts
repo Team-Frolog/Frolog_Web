20edaff196046a7e76dc1aaae6edafa2658fa3cb
JIMIN1020 <jimins4920@gmail.com>
2024-10-31 11:48:42 +0900

    [File/#66]: 우물 물결 svg 추가 및 세팅

--- src/constants/category.ts
+++ src/constants/category.ts
@@ -1,9 +1,12 @@
+const BASE_URL = '/images/well/wave';
+
 interface Category {
   [key: string]: {
     name: string;
     bg: string;
     text: string;
     band: string;
+    wave: string;
   };
 }
 
@@ -13,89 +16,104 @@ export const CATEGORY: Category = {
     bg: '#FE2F2F',
     text: '#FABBC8',
     band: '#FF7171',
+    wave: `${BASE_URL}/religion.svg`,
   },
   art: {
     name: '예술',
     bg: '#FE49A9',
     text: '#FFB5DC',
     band: '#FF7CC2',
+    wave: `${BASE_URL}/art.svg`,
   },
   cartoon: {
     name: '만화',
     bg: '#FF8BCA',
     text: '#AE4039',
     band: '#FFADDA',
+    wave: `${BASE_URL}/cartoon.svg`,
   },
   humanities: {
     name: '인문학',
     bg: '#B85FFF',
     text: '#F3B3EE',
     band: '#D095FF',
+    wave: `${BASE_URL}/humanities.svg`,
   },
   novel: {
     name: '소설',
     bg: '#CAA2FF',
     text: '#6C5A84',
     band: '#D9BBFF',
+    wave: `${BASE_URL}/novel.svg`,
   },
   foreign_languages: {
     name: '외국어',
     bg: '#92666C',
     text: '#FFC5CC',
     band: '#B78E93',
+    wave: `${BASE_URL}/foreign_languages.svg`,
   },
   self_development: {
     name: '자기개발',
     bg: '#FB7A35',
     text: '#FFC8AB',
     band: '#FFA474',
+    wave: `${BASE_URL}/self_development.svg`,
   },
   sociology: {
     name: '사회',
     bg: '#FFA144',
     text: '#FFDDBB',
     band: '#FFC183',
+    wave: `${BASE_URL}/sociology.svg`,
   },
   unknown: {
     name: '기타',
     bg: '#FFDA4A',
     text: '#A78500',
     band: '#FFE88C',
+    wave: `${BASE_URL}/unknown.svg`,
   },
   science: {
     name: '과학',
     bg: '#4D6FF4',
     text: '#BBC4E9',
     band: '#9BACF1',
+    wave: `${BASE_URL}/science.svg`,
   },
   it: {
     name: 'IT',
     bg: '#84ADFF',
     text: '#3C4F77',
     band: '#AAC7FF',
+    wave: `${BASE_URL}/it.svg`,
   },
   economic_business: {
     name: '경제경영',
     bg: '#6BCEF5',
     text: '#0073A0',
     band: '#9DE3FF',
+    wave: `${BASE_URL}/economic_business.svg`,
   },
   life: {
     name: '라이프',
     bg: '#75F3C9',
     text: '#00754E',
     band: '#B0FFE5',
+    wave: `${BASE_URL}/life.svg`,
   },
   travel: {
     name: '여행',
     bg: '#00CE4C',
     text: '#00722A',
     band: '#6EED9C',
+    wave: `${BASE_URL}/travel.svg`,
   },
   essay: {
     name: '에세이',
     bg: '#A0FF56',
     text: '#419400',
     band: '#CAFFA0',
+    wave: `${BASE_URL}/essay.svg`,
   },
 };
