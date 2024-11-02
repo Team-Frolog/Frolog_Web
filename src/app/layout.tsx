ef0fa1c8119b7c3ddc74890141b03839693ac5b3
JIMIN1020 <jimins4920@gmail.com>
2024-11-02 19:03:17 +0900

    [Feat/#67]: PWA splash screen 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,87 @@ export const metadata: Metadata = {
   description: '책을 추가해 우물을 탈출하는 독서 기록 서비스, 프롤로그(Frolog)',
   viewport:
     'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no viewport-fit=cover',
+  appleWebApp: {
+    capable: true,
+    title: '프롤로그',
+    statusBarStyle: 'black-translucent',
+  },
   icons: {
     icon: '/logo/favicon.ico',
+    other: [
+      {
+        url: '/splash/iPhone_14__iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_portrait.png',
+        media:
+          'screen and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+      {
+        url: '/splash/iPhone_11__iPhone_XR_portrait.png',
+        media:
+          'screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+      {
+        url: '/splash/iPhone_14_Plus__iPhone_13_Pro_Max__iPhone_12_Pro_Max_portrait.png',
+        media:
+          'screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+      {
+        url: '/splash/iPhone_16_Pro_portrait.png',
+        media:
+          'screen and (device-width: 402px) and (device-height: 874px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+      {
+        url: '/splash/4__iPhone_SE__iPod_touch_5th_generation_and_later_portrait.png',
+        media:
+          'screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+      {
+        url: '/splash/iPhone_11_Pro_Max__iPhone_XS_Max_portrait.png',
+        media:
+          'screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+      {
+        url: '/splash/iPhone_16__iPhone_15_Pro__iPhone_15__iPhone_14_Pro_portrait.png',
+        media:
+          'screen and (device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+      {
+        url: '/splash/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_portrait.png',
+        media:
+          'screen and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+      {
+        url: '/splash/iPhone_16_Plus__iPhone_15_Pro_Max__iPhone_15_Plus__iPhone_14_Pro_Max_portrait.png',
+        media:
+          'screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+      {
+        url: '/splash/iPhone_16_Pro_Max_portrait.png',
+        media:
+          'screen and (device-width: 440px) and (device-height: 956px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+      {
+        url: '/splash/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_portrait.png',
+        media:
+          'screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+      {
+        url: '/splash/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_portrait.png',
+        media:
+          'screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+    ],
   },
 };
 
@@ -29,12 +108,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang='ko'>
-      <head>
-        <meta
-          name='apple-mobile-web-app-status-bar-style'
-          content='black-translucent'
-        />
-      </head>
       <QueryProvider>
         <ThemeProvider />
         <AuthProvider>
