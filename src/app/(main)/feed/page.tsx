cfb978e7cee9c0349d6e33dc8b92eeeb26a95cb8
JIMIN1020 <jimins4920@gmail.com>
2024-11-02 15:21:45 +0900

    [Style/#68]: 피드 pull to refresh 수정

--- src/app/(main)/feed/page.tsx
+++ src/app/(main)/feed/page.tsx
@@ -12,14 +12,20 @@ function FeedPage() {
   return (
     <>
       <SideHeader />
-      <MainLayout ref={containerRef} extraClass='bg-gray-300'>
-        <PullToRefresh element={containerRef} />
-        <div className='flex h-fit w-full px-page py-[20px] pt-[50px]'>
-          <h1 className='w-fit max-w-[250px] text-start text-heading-md-bold'>
-            피드
-          </h1>
+
+      <MainLayout extraClass='bg-gray-300 overflow-hidden'>
+        <div
+          ref={containerRef}
+          className='flex w-full flex-1 flex-col overflow-auto scrollbar-hide'
+        >
+          <PullToRefresh element={containerRef} />
+          <div className='flex h-fit w-full px-page py-[20px] pt-[50px]'>
+            <h1 className='w-fit max-w-[250px] text-start text-heading-md-bold'>
+              피드
+            </h1>
+          </div>
+          <FeedList />
         </div>
-        <FeedList />
       </MainLayout>
     </>
   );
