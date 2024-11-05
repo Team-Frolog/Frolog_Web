ab520303471bf99c3ecef5c52fcbde70d1ade5f3
JIMIN1020 <jimins4920@gmail.com>
2024-11-06 00:49:31 +0900

    [Fix/#72]: 미션 순서 변경

--- src/app/(store)/mission/page.tsx
+++ src/app/(store)/mission/page.tsx
@@ -22,20 +22,6 @@ function MissionPage() {
     <>
       <div className='flex w-full flex-col gap-[20px]'>
         <div className='flex flex-col gap-[4px] rounded-[20px] bg-[#FF8BCA] px-[20px] pt-[20px] text-white'>
-          <h5 className='text-title-xl-bold'>읽는 중 책 추가</h5>
-          <div className='flex flex-col'>
-            <span className='text-body-lg-bold'>1일 최대 20p</span>
-            <span className='self-end text-body-lg'>1회 10p</span>
-            <Image
-              src={IMAGES.frog.add.reading}
-              alt='reading book frog'
-              width={127}
-              height={62}
-              className='self-end pt-[16px]'
-            />
-          </div>
-        </div>
-        <div className='flex flex-col gap-[4px] rounded-[20px] bg-[#85DCFF] px-[20px] pt-[20px] text-white'>
           <h5 className='text-title-xl-bold'>다 읽은 책 추가</h5>
           <div className='flex flex-col'>
             <span className='text-body-lg-bold'>1일 최대 20p</span>
@@ -43,6 +29,20 @@ function MissionPage() {
             <Image
               src={IMAGES.frog.add.done}
               alt='read book frog'
+              width={127}
+              height={62}
+              className='self-end pt-[16px]'
+            />
+          </div>
+        </div>
+        <div className='flex flex-col gap-[4px] rounded-[20px] bg-[#85DCFF] px-[20px] pt-[20px] text-white'>
+          <h5 className='text-title-xl-bold'>읽는 중 책 추가</h5>
+          <div className='flex flex-col'>
+            <span className='text-body-lg-bold'>1일 최대 20p</span>
+            <span className='self-end text-body-lg'>1회 10p</span>
+            <Image
+              src={IMAGES.frog.add.reading}
+              alt='reading book frog'
               width={127}
               height={62}
               className='self-end pt-[16px]'
