9736bd03214561568773a87ed005ab31b0fa3b37
JIMIN1020 <jimins4920@gmail.com>
2024-11-06 19:15:45 +0900

    [Style/#72]: 로그인 전 페이지 디자인 변경

--- src/app/(main)/default/page.tsx
+++ src/app/(main)/default/page.tsx
@@ -9,6 +9,7 @@ import MainLayout from '@/layouts/MainLayout';
 import React from 'react';
 import WellBubble from 'public/images/well/well-bubble.svg';
 import Image from 'next/image';
+import { CATEGORY } from '@/constants/category';
 
 function DefaultWellPage() {
   return (
@@ -25,9 +26,9 @@ function DefaultWellPage() {
         />
         <div className='flex w-full flex-col-reverse items-center'>
           <div className='flex w-full flex-col'>
-            <div className='relative z-auto box-border flex h-[55px] w-full justify-center bg-[#A6B4D3] pt-[12px]'>
+            <div className='relative z-auto box-border flex h-[55px] w-full justify-center bg-category-bg-life pt-[12px]'>
               <Image
-                src='/images/well/wave/default-2.svg'
+                src={CATEGORY.life.wave}
                 alt='wave'
                 width={390}
                 height={12}
@@ -35,24 +36,24 @@ function DefaultWellPage() {
                 loading='eager'
               />
               <WellBubble
-                fill='#C0CEED'
+                fill={CATEGORY.life.band}
                 className='absolute left-[24px] top-[8px]'
               />
-              <span className='text-body-sm-bold text-gray-600'>
-                읽고 있는 도서는 이렇게 기포가 생겨나요!
+              <span className='text-body-sm-bold text-category-text-life'>
+                읽는 중인 책은 이렇게 기포가 생겨요!
               </span>
             </div>
-            <div className='relative z-auto box-border flex h-[55px] w-full justify-center bg-[#B7CEFF] pt-[12px]'>
+            <div className='relative z-auto box-border flex h-[55px] w-full justify-center bg-category-bg-economic_business pt-[12px]'>
               <Image
-                src='/images/well/wave/default.svg'
+                src={CATEGORY.economic_business.wave}
                 alt='wave'
                 width={390}
                 height={12}
                 className='absolute -top-[12px] left-0 h-[12px] w-full'
                 loading='eager'
               />
-              <span className='text-body-sm-bold text-[#727384]'>
-                다 읽은 책은 이렇게 쌓여요
+              <span className='text-body-sm-bold text-category-text-economic_business'>
+                책을 추가해 높이 올라가봐요!
               </span>
             </div>
           </div>
