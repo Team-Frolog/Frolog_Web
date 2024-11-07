5c15c9c59607a778a62d52f49f99d38845817a22
JIMIN1020 <jimins4920@gmail.com>
2024-11-07 22:11:29 +0900

    [Fix/#64]: getFrogs -> searchStoreItem으로 변경

--- src/features/Well/hooks/useFrogs.ts
+++ src/features/Well/hooks/useFrogs.ts
@@ -1,11 +1,12 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
-import { getFrogList } from '../api/frog.api';
+// import { getFrogList } from '../api/frog.api';
+import { getStoreItems } from '@/features/Store/api/store.api';
 
 export const useFrogs = (userId: string) => {
   const { data } = useSuspenseQuery({
     queryKey: ['frogs', userId],
-    queryFn: () => getFrogList(userId),
+    queryFn: () => getStoreItems({ owner: userId }),
   });
 
-  return { frogs: data };
+  return { frogs: data.items };
 };
