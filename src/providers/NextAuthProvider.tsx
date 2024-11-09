00108ea80ebbaccdb5492b27700456d5c5506a96
JIMIN1020 <jimins4920@gmail.com>
2024-11-09 14:35:22 +0900

    [Fix/#75]: token handler 누락 수정

--- src/providers/NextAuthProvider.tsx
+++ src/providers/NextAuthProvider.tsx
@@ -1,10 +1,18 @@
 'use client';
 
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, useSession } from 'next-auth/react';
 import React from 'react';
+import { TokenHandler } from './TokenHandler';
 
 function NextAuthProvider({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  const { data: session, update } = useSession();
+
+  return (
+    <SessionProvider>
+      {children}
+      <TokenHandler session={session} update={update} />
+    </SessionProvider>
+  );
 }
 
 export default NextAuthProvider;
