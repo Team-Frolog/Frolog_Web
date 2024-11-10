c8219feeb9b07fee82a691260a5bf3e239496eab
JIMIN1020 <jimins4920@gmail.com>
2024-11-10 22:37:47 +0900

    [Feat/#78]: session store 생성

---
+++ src/store/sessionStore.ts
@@ -0,0 +1,49 @@
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+
+interface Session {
+  userId: string | null;
+  accessToken: string | null;
+  defaultWellId: string | null;
+}
+
+interface SessionStore {
+  userId: string | null;
+  accessToken: string | null;
+  defaultWellId: string | null;
+  setSession: (session: Session) => void;
+}
+
+const useSessionStore = create<SessionStore>()(
+  persist(
+    (set) => ({
+      userId: null,
+      accessToken: null,
+      defaultWellId: null,
+      setSession: () =>
+        set((session) => ({
+          userId: session.userId,
+          accessToken: session.accessToken,
+          defaultWellId: session.defaultWellId,
+        })),
+    }),
+    {
+      name: 'SessionStore',
+      storage: createJSONStorage(() => sessionStorage),
+      partialize: (state) => ({
+        userId: state.userId,
+        accessToken: state.accessToken,
+        defaultWellId: state.defaultWellId,
+        setSession: state.setSession,
+      }),
+    }
+  )
+);
+
+export const useUserId = () => useSessionStore((state) => state.userId);
+export const useAccessToken = () =>
+  useSessionStore((state) => state.accessToken);
+export const useDefaultWell = () =>
+  useSessionStore((state) => state.defaultWellId);
+
+export default useSessionStore;
