412efa0d35091e203a87fcec5c4a7d4780bbe74d
JIMIN1020 <jimins4920@gmail.com>
2024-11-19 11:11:24 +0900

    [Fix/#87]: middleware 내 토큰 재발급 로직 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
-import { getToken } from 'next-auth/jwt';
+import { RefreshToken } from '@frolog/frolog-api';
+import { encode, getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { getExpFromToken } from './utils/auth/decodeToken';
 
 const protectedRoutes: string[] = [
   '/frolog-test',
@@ -24,7 +26,7 @@ const publicRoutes: string[] = [
 ]; // 로그인이 되면 접근할 수 없는 페이지 목록
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({
+  const sessionToken = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET,
   });
@@ -33,8 +35,11 @@ export async function middleware(req: NextRequest) {
   const isRemember = req.cookies.get('isRemember');
   const isLoggedIn = req.cookies.get('isLoggedIn');
 
-  if (token && isRemember?.value === 'false' && !isLoggedIn) {
-    const response = NextResponse.redirect(new URL('/default', req.url));
+  let response = NextResponse.next();
+  let newSessionToken = undefined;
+
+  if (sessionToken && isRemember?.value === 'false' && !isLoggedIn) {
+    response = NextResponse.redirect(new URL('/default', req.url));
     response.cookies.set(process.env.NEXTAUTH_TOKEN_NAME || '', '', {
       httpOnly: true,
       secure: true,
@@ -45,16 +50,72 @@ export async function middleware(req: NextRequest) {
     return response;
   }
 
+  // 토큰 만료 확인 후 재발급
+  if (sessionToken) {
+    const nowTime = Math.floor(new Date().getTime() / 1000);
+    const timeRemaining = sessionToken.accessTokenExpires - nowTime;
+
+    // 만료된 경우
+    if (timeRemaining <= 0) {
+      // 재발급
+      const result = await new RefreshToken({
+        baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+        accessToken: sessionToken.accessToken,
+      }).fetch({ refresh_token: sessionToken.refreshToken });
+
+      if (result.access_token && result.refresh_token) {
+        // 새로운 토큰 세팅
+        newSessionToken = await encode({
+          token: {
+            ...sessionToken,
+            refreshToken: result.refresh_token,
+            accessToken: result.access_token,
+            accessTokenExpires: getExpFromToken(result.access_token),
+          },
+          secret: process.env.NEXTAUTH_SECRET!,
+          maxAge: 30 * 24 * 60 * 60,
+        });
+
+        response.cookies.set(
+          process.env.NEXTAUTH_TOKEN_NAME!,
+          newSessionToken,
+          {
+            httpOnly: true,
+            secure: true,
+            path: '/',
+            sameSite: 'lax',
+            maxAge: 30 * 24 * 60 * 60,
+          }
+        );
+      }
+    }
+  }
+
   const { pathname } = req.nextUrl;
-  const defaultWellId = token ? token.defaultWellId : undefined;
+  const defaultWellId = sessionToken ? sessionToken.defaultWellId : undefined;
 
   if (pathname === '/') {
-    if (!token) {
+    if (!sessionToken) {
       return NextResponse.redirect(new URL('/default', req.url));
     } else if (defaultWellId) {
-      return NextResponse.redirect(
-        new URL(`/${token.id}/well/${defaultWellId}`, req.url)
+      // 재발급
+      const redirectResponse = NextResponse.redirect(
+        new URL(`/${sessionToken.id}/well/${defaultWellId}`, req.url)
       );
+      if (newSessionToken) {
+        redirectResponse.cookies.set(
+          process.env.NEXTAUTH_TOKEN_NAME!,
+          newSessionToken,
+          {
+            httpOnly: true,
+            secure: true,
+            path: '/',
+            sameSite: 'lax',
+            maxAge: 30 * 24 * 60 * 60,
+          }
+        );
+      }
+      return redirectResponse;
     }
   }
 
@@ -63,24 +124,51 @@ export async function middleware(req: NextRequest) {
   const isWithOutAuth = publicRoutes.some((route) => pathname.includes(route));
 
   // 로그인 여부에 따라 redirect
-  if (isWithAuth) return withAuth(req, !!token);
-  else if (isWithOutAuth) return withOutAuth(req, !!token);
+  if (isWithAuth) return withAuth(req, !!sessionToken, response);
+  else if (isWithOutAuth)
+    return withOutAuth(req, !!sessionToken, response, newSessionToken);
+
+  return response;
 }
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images|$).*)', '/'],
 };
 
-function withAuth(req: NextRequest, isLoggedIn: boolean) {
+function withAuth(
+  req: NextRequest,
+  isLoggedIn: boolean,
+  response: NextResponse
+) {
   if (!isLoggedIn) {
     return NextResponse.redirect(new URL('/onboarding', req.url));
   }
-  return NextResponse.next();
+  return response;
 }
 
-function withOutAuth(req: NextRequest, isLoggedIn: boolean) {
+function withOutAuth(
+  req: NextRequest,
+  isLoggedIn: boolean,
+  response: NextResponse,
+  newSessionToken: string | undefined
+) {
   if (isLoggedIn) {
-    return NextResponse.redirect(new URL('/', req.url));
+    const redirectResponse = NextResponse.redirect(new URL('/', req.url));
+
+    if (newSessionToken) {
+      redirectResponse.cookies.set(
+        process.env.NEXTAUTH_TOKEN_NAME!,
+        newSessionToken,
+        {
+          httpOnly: true,
+          secure: true,
+          path: '/',
+          sameSite: 'lax',
+          maxAge: 30 * 24 * 60 * 60,
+        }
+      );
+    }
+    return redirectResponse;
   }
-  return NextResponse.next();
+  return response;
 }
