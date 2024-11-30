13e5421c1570a6c1718302ce26eb1792a58644cb
JIMIN1020 <jimins4920@gmail.com>
2024-11-30 12:44:58 +0900

    [File/#95]: 실루엣 세팅, 기존 캐릭터 파일명 변경

--- src/constants/frogs.ts
+++ src/constants/frogs.ts
@@ -10,62 +10,62 @@ interface Frog {
 
 export const FROGS: Frog = {
   default: {
-    src: `${BASE_PATH}/frog/character/square/default-square.webp`,
+    src: `${BASE_PATH}/frog/character/default.webp`,
     name: '개꾸리',
     marginBottom: 0,
   },
   fro: {
-    src: `${BASE_PATH}/frog/character/square/fro-square.webp`,
+    src: `${BASE_PATH}/frog/character/fro.webp`,
     name: '프로',
     marginBottom: 0,
   },
   roro: {
-    src: `${BASE_PATH}/frog/character/square/roro-square.webp`,
+    src: `${BASE_PATH}/frog/character/roro.webp`,
     name: '로로',
     marginBottom: 0,
   },
   rogy: {
-    src: `${BASE_PATH}/frog/character/square/rogy-square.webp`,
+    src: `${BASE_PATH}/frog/character/rogy.webp`,
     name: '로기',
     marginBottom: 0,
   },
   ghost: {
-    src: `${BASE_PATH}/frog/character/square/ghost-square.webp`,
+    src: `${BASE_PATH}/frog/character/ghost.webp`,
     name: '유령꾸리',
     marginBottom: 0,
   },
   mummy: {
-    src: `${BASE_PATH}/frog/character/square/mummy-square.webp`,
+    src: `${BASE_PATH}/frog/character/mummy.webp`,
     name: '미라꾸리',
     marginBottom: 0,
   },
   pumpkin: {
-    src: `${BASE_PATH}/frog/character/square/pumpkin-square.webp`,
+    src: `${BASE_PATH}/frog/character/pumpkin.webp`,
     name: '호박꾸리',
     marginBottom: 0,
   },
   skeleton: {
-    src: `${BASE_PATH}/frog/character/square/skeleton-square.webp`,
+    src: `${BASE_PATH}/frog/character/skeleton.webp`,
     name: '해골꾸리',
     marginBottom: 0,
   },
   devil: {
-    src: `${BASE_PATH}/frog/character/square/devil-square.webp`,
+    src: `${BASE_PATH}/frog/character/devil.webp`,
     name: '악마꾸리',
     marginBottom: 0,
   },
   gguristein: {
-    src: `${BASE_PATH}/frog/character/square/gguristein-square.webp`,
+    src: `${BASE_PATH}/frog/character/gguristein.webp`,
     name: '꾸리슈타인',
     marginBottom: 0,
   },
   witch: {
-    src: `${BASE_PATH}/frog/character/square/witch-square.webp`,
+    src: `${BASE_PATH}/frog/character/witch.webp`,
     name: '마녀꾸리',
     marginBottom: 20,
   },
   dracula: {
-    src: `${BASE_PATH}/frog/character/square/dracula-square.webp`,
+    src: `${BASE_PATH}/frog/character/dracula.webp`,
     name: '꾸리백작',
     marginBottom: 10,
   },
@@ -126,14 +126,14 @@ export const FROGS_SILHOUETTE: {
   witch: `${BASE_PATH}/frog/character/silhouette/witch-silhouette.webp`,
   dracula: `${BASE_PATH}/frog/character/silhouette/dracula-silhouette.webp`,
   // 크리스마스
-  gguristmas: `${BASE_PATH}/christmas/character/silhouette/`,
-  box: `${BASE_PATH}/christmas/character/silhouette/`,
-  candle: `${BASE_PATH}/christmas/character/silhouette/`,
-  gguribread: `${BASE_PATH}/christmas/character/silhouette/`,
-  snowgguri: `${BASE_PATH}/christmas/character/silhouette/`,
-  santa: `${BASE_PATH}/christmas/character/silhouette/`,
-  tree: `${BASE_PATH}/christmas/character/silhouette/`,
-  ggudolph: `${BASE_PATH}/christmas/character/silhouette/`,
+  gguristmas: `${BASE_PATH}/christmas/character/silhouette/gguristmas-silhouette.webp`,
+  box: `${BASE_PATH}/christmas/character/silhouette/box-silhouette.webp`,
+  candle: `${BASE_PATH}/christmas/character/silhouette/candle-silhouette.webp`,
+  gguribread: `${BASE_PATH}/christmas/character/silhouette/gguribread-silhouette.webp`,
+  snowgguri: `${BASE_PATH}/christmas/character/silhouette/snowgguri-silhouette.webp`,
+  santa: `${BASE_PATH}/christmas/character/silhouette/santa-silhouette.webp`,
+  tree: `${BASE_PATH}/christmas/character/silhouette/tree-silhouette.webp`,
+  ggudolph: `${BASE_PATH}/christmas/character/silhouette/ggudolph-silhouette.webp`,
 };
 
 export const SHEET_FROG = {
