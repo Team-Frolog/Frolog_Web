3296c6e5795547a3a75a131b2ac2d26f45661f50
JIMIN1020 <jimins4920@gmail.com>
2024-12-04 19:10:24 +0900

    [Test/#94]: Button 테스트코드 구축

---
+++ src/__test__/components/Button/Button.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import Button from '@/components/Button/Button';
+import '@testing-library/jest-dom';
+
+test('버튼이 클릭되면 onClick 핸들러를 실행한다.', () => {
+  const handleClick = jest.fn();
+
+  render(<Button onClick={handleClick}>Click Me</Button>);
+  const button = screen.getByRole('button');
+
+  fireEvent.click(button);
+  expect(handleClick).toHaveBeenCalledTimes(1);
+});
+
+test('버튼이 disabled 인 경우, 클릭이 동작하지 않아야 한다.', () => {
+  const handleClick = jest.fn();
+
+  render(
+    <Button onClick={handleClick} disabled>
+      Click Me
+    </Button>
+  );
+  const button = screen.getByRole('button');
+
+  fireEvent.click(button);
+  expect(handleClick).not.toHaveBeenCalled();
+});
+
+test('버튼에는 props로 전달한 type이 적용되어야 한다.', () => {
+  const type = 'submit';
+
+  render(<Button type={type}>Submit</Button>);
+  const button = screen.getByRole('button');
+
+  expect(button).toHaveAttribute('type', 'submit');
+});
+
+test('theme이 전달되면 적절한 class가 적용되어야 한다.', () => {
+  const theme = 'error';
+
+  render(<Button theme={theme}>Error Button</Button>);
+  const button = screen.getByRole('button');
+
+  expect(button).toHaveClass('button-error');
+});
