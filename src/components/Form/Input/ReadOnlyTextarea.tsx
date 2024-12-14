3046a5382f01138a8ec723942affd03f4624f50e
JIMIN1020 <jimins4920@gmail.com>
2024-12-14 18:59:30 +0900

    [Test/#94]: Textarea 리팩토링 및 테스트 코드 작성

--- src/components/Form/Input/ReadOnlyTextarea.tsx
+++ src/components/Form/Input/ReadOnlyTextarea.tsx
@@ -2,11 +2,17 @@ import { TextareaType } from '@/data/ui/textareaType';
 import React from 'react';
 
 interface Props {
+  /** textarea 데이터 */
   option: TextareaType;
+  /** 컨텐츠 */
   content: string;
+  /** 폰트 타입 - 기본/굵게 */
   type?: 'default' | 'bold';
 }
 
+/** 리뷰, 메모에 활용되는 read only textarea
+ * - 컨텐츠 내용에 따라 높이가 조절됩니다.
+ */
 function ReadOnlyTextarea({ option, content, type }: Props) {
   return (
     <div className='flex w-full flex-col gap-[8px]'>
