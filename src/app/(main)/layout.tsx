9922bb53e4061a7652d6f274b458fea15c5c8d4c
JIMIN1020 <jimins4920@gmail.com>
2025-01-28 23:52:04 +0900

    [Refactor/#118]: main layout > div에서 fragment로 변경 및 파일 이동

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='safe-screen flex w-full flex-col items-center overflow-hidden'>
+    <>
       {children}
       <NavigationBar />
-    </div>
+    </>
   );
 }
 
