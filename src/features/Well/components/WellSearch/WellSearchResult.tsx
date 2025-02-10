9b1a9ef313db59f31fb4ff9a23a9d62acc844ae4
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-02-11 05:15:10 +0900

    [Feat/#120]: 우물 검색 기능 구현

--- src/features/Well/components/WellSearch/WellSearchResult.tsx
+++ src/features/Well/components/WellSearch/WellSearchResult.tsx
@@ -1,13 +1,41 @@
+'use client';
+
 import React from 'react';
 import MainLayout from '@/layouts/MainLayout';
+import { useSearchWells } from '@/features/Well/hooks/useSearchWells';
+import WellSearchItem from '@/features/Well/components/WellSearch/WellSearchItem';
+import { useObserver } from '@/hooks/gesture/useObserver';
+import WellSearchItemSkeleton from '@/components/Fallback/Skeleton/WellSearchItemSkeleton';
 
 function WellSearchResult() {
+  const {
+    searchResult,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isFetchingNextPage,
+  } = useSearchWells();
+  const { setTarget } = useObserver({
+    hasNextPage,
+    fetchNextPage,
+  });
+
+  if (!searchResult) return null;
+
   return (
     <MainLayout extraClass='gap-[36px] pb-[36px] pt-[24px] bg-gray-300'>
-      {/* <WellSearchItem />
-      <WellSearchItem />
-      <WellSearchItem /> */}
-      <h1>search</h1>
+      {searchResult.map((resultItem) => (
+        <WellSearchItem
+          key={resultItem.id}
+          userId={resultItem.id}
+          wells={resultItem.wells}
+        />
+      ))}
+      {isFetchingNextPage || isLoading ? (
+        <WellSearchItemSkeleton />
+      ) : (
+        <div ref={setTarget} id='observer' className='h-[10px]' />
+      )}
     </MainLayout>
   );
 }
