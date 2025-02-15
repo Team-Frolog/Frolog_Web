b6c78fdf2f77cafceaa2e4a3b50d3ee6b4822ad6
JIMIN1020 <jimins4920@gmail.com>
2025-02-15 22:02:29 +0900

    [Comment/#137]: useScrollPosition 주석 추가

--- src/hooks/gesture/useScrollPosition.ts
+++ src/hooks/gesture/useScrollPosition.ts
@@ -6,14 +6,18 @@ import {
 } from '@/store/userActionStore';
 
 interface Props {
+  /** 스크롤 위치를 저장할 곳의 키값 */
   key: keyof LastScrollPos;
+  /** 스크롤 위치 저장 조건 */
   condition: boolean;
 }
 
+/** 스크롤 유지를 위해 스크롤 위치를 저장하고, 이동시키는 훅 */
 export const useScrollPosition = ({ key, condition }: Props) => {
   const lastScrollPos = useScrollPos();
   const { setScrollPos } = useUserActionActions();
 
+  /** 스크롤 위치 이동 */
   useEffect(() => {
     if (!lastScrollPos[key] || !condition) return;
 
@@ -25,6 +29,7 @@ export const useScrollPosition = ({ key, condition }: Props) => {
     }
   }, [condition]);
 
+  /** 스크롤 저장 함수 */
   const saveScroll = () => {
     const main = window.document.getElementById('main');
     setScrollPos(main?.scrollTop ?? null, key);
