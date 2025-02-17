af8559f49ccfbed25504a71361714686bcd3f503
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-02-18 00:58:00 +0900

    [Api/#120]: explore 페이지 api 연동

--- src/app/(main)/(well)/explore/page.tsx
+++ src/app/(main)/(well)/explore/page.tsx
@@ -1,21 +1,57 @@
+import WellListSkeleton from '@/components/Fallback/Skeleton/WellListSkeleton';
+import { DEFAULT_LIMIT } from '@/constants/api';
 import { PAGES } from '@/constants/page';
+import { QUERY_KEY } from '@/constants/query';
 import { SearchInput } from '@/features/Search';
-import WellExploreList from '@/features/Well/components/WellSearch/WellExploreList';
+import { authOptions } from '@/utils/auth/nextAuth';
+import { SearchUserWell } from '@frolog/frolog-api';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+import { getServerSession } from 'next-auth';
+import dynamic from 'next/dynamic';
 import React from 'react';
 
-function ExplorePage() {
+const WellExploreList = dynamic(
+  () => import('@/features/Well/components/WellSearch/WellExploreList'),
+  {
+    ssr: false,
+    loading: () => <WellListSkeleton />,
+  }
+);
+
+async function ExplorePage() {
+  const session = await getServerSession(authOptions);
+  const userId = session?.user.id;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: [QUERY_KEY.explore, userId],
+    queryFn: ({ pageParam }) =>
+      new SearchUserWell({
+        baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+        accessToken: session?.user.accessToken,
+      }).fetch({ limit: DEFAULT_LIMIT, page: pageParam }),
+    initialPageParam: 0,
+    staleTime: 1000 * 10,
+  });
+
   return (
-    <div className='flex w-full flex-col'>
-      <div className='flex px-[24px] pb-[20px]'>
-        <SearchInput
-          route={PAGES.WELL_SEARCH}
-          placeholder='우물 키워드를 검색해보세요'
-        />
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className='flex w-full flex-col'>
+        <div className='flex px-[24px] pb-[20px]'>
+          <SearchInput
+            route={PAGES.WELL_SEARCH}
+            placeholder='우물 키워드를 검색해보세요'
+          />
+        </div>
+        <div className='flex w-full flex-col gap-[36px]'>
+          <WellExploreList />
+        </div>
       </div>
-      <div className='flex w-full flex-col gap-[36px]'>
-        <WellExploreList />
-      </div>
-    </div>
+    </HydrationBoundary>
   );
 }
 
--- src/features/Well/components/WellSearch/WellExploreList.tsx
+++ src/features/Well/components/WellSearch/WellExploreList.tsx
@@ -1,48 +1,30 @@
 'use client';
 
 import WellSearchItemSkeleton from '@/components/Fallback/Skeleton/WellSearchItemSkeleton';
-import { QUERY_KEY } from '@/constants/query';
 import WellSearchItem from '@/features/Well/components/WellSearch/WellSearchItem';
+import { useExploreWells } from '@/features/Well/hooks/useExploreWells';
 import { useObserver } from '@/hooks/gesture/useObserver';
-import { useInfiniteQuery } from '@tanstack/react-query';
 import React from 'react';
 
-const fetchExplore = async (page: number) => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/v1/well/userwell/search?page=${page}`
-  ).then((res) => res.json());
-
-  return response;
-};
-
 function WellExploreList() {
-  const { data, fetchNextPage, hasNextPage, isLoading, isFetchingNextPage } =
-    useInfiniteQuery({
-      queryKey: [QUERY_KEY.explore],
-      queryFn: ({ pageParam }) => fetchExplore(pageParam),
-      initialPageParam: 0,
-      getNextPageParam: (lastPage) => {
-        const totalPages = Math.ceil(lastPage.count / lastPage.limit);
-        const isLastPage = totalPages === lastPage.page + 1 || totalPages === 0;
-        return isLastPage ? undefined : lastPage.page + 1;
-      },
-      select: (fetchedData) => ({
-        pages: fetchedData
-          ? fetchedData.pages.flatMap((page) => page.wells)
-          : [],
-        pageParams: fetchedData.pageParams,
-      }),
-    });
+  const {
+    exploreResult,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isFetchingNextPage,
+  } = useExploreWells();
+
   const { setTarget } = useObserver({
     hasNextPage,
     fetchNextPage,
   });
 
-  if (!data || isLoading) return <WellSearchItemSkeleton />;
+  if (!exploreResult || isLoading) return <WellSearchItemSkeleton />;
 
   return (
     <div className='flex w-full flex-col gap-[36px]'>
-      {data.pages.map(({ id, wells }) => (
+      {exploreResult.pages.map(({ id, wells }) => (
         <WellSearchItem key={id} userId={id} wells={wells} />
       ))}
       {isFetchingNextPage || isLoading ? (
