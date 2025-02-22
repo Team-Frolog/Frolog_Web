bd825bb3e04d4b3bb1f8e01b25d586d7e5910370
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-02-23 00:11:18 +0900

    [Refactor/#120]: 우물 탐색 컴포넌트 Hydration Boundary 적용

--- src/app/(main)/(well)/explore/page.tsx
+++ src/app/(main)/(well)/explore/page.tsx
@@ -44,19 +44,19 @@ async function ExplorePage() {
   });
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <div className='flex w-full flex-col'>
-        <div className='flex px-[24px] pb-[20px]'>
-          <SearchInput
-            route={PAGES.WELL_SEARCH}
-            placeholder='우물 키워드를 검색해보세요'
-          />
-        </div>
-        <div className='flex w-full flex-col gap-[36px]'>
-          <WellExploreList refTime={refTime} />
-        </div>
+    <div className='flex w-full flex-col'>
+      <div className='flex px-[24px] pb-[20px]'>
+        <SearchInput
+          route={PAGES.WELL_SEARCH}
+          placeholder='우물 키워드를 검색해보세요'
+        />
       </div>
-    </HydrationBoundary>
+      <div className='flex w-full flex-col gap-[36px]'>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <WellExploreList refTime={refTime} />
+        </HydrationBoundary>
+      </div>
+    </div>
   );
 }
 
