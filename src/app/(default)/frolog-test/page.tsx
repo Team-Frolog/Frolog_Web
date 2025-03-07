f25f0dcd88f240b25e10dfdfe6b07e151906c97f
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-03-07 15:57:26 +0900

    [Refactor/#150]: 사용자 노출이 적은 페이지 dynamic 적용

--- src/app/(default)/frolog-test/page.tsx
+++ src/app/(default)/frolog-test/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { FrologTest } from '@/features/FrologTest';
 import { Metadata } from 'next';
+import dynamic from 'next/dynamic';
 
 export const metadata: Metadata = {
   title: '독서 성향 테스트',
 };
+
+const FrologTest = dynamic(
+  () => import('@/features/FrologTest/components/FrologTest')
+);
 
 function TestPage() {
   return <FrologTest />;
