dfc7757eb0930bce00008164fca300af914e7913
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-06-20 00:16:54 +0900

    [Chore/#185]: 목 api 응답값 수정

--- src/app/api/mock/route.ts
+++ src/app/api/mock/route.ts
@@ -8,6 +8,7 @@ export async function POST(request: NextRequest) {
     email: 'test@test.com',
     is_registerd: false,
     login_type: null,
-    email_verified_token: 'test',
+    email_verified_token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZ1bGZpbGxlZDc0MThAZ21haWwuY29tIiwidGFyZ2V0Ijoic2lnblVwIiwiaWF0IjoxNzUwMzQzNjI2LCJleHAiOjE3NTA0MzAwMjYsInN1YiI6InZlcmlmeUVtYWlsQ29kZSJ9.WDfsAF3npSC3NKkOSOjmtkKazGIY4gfneFxz6zs2-qE',
   });
 }
