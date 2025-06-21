5f24ab8a24df507859812a773328cbe0b9e9dee9
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-06-21 23:10:06 +0900

    [Feat/#188]: 프로필 피드 아이템 삭제후 데이터 최신화

--- src/features/Profile/hooks/useDeleteProfileFeedItem.ts
+++ src/features/Profile/hooks/useDeleteProfileFeedItem.ts
@@ -1,15 +1,17 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteProfileFeedItem } from '../api/feed.api';
 import { useParams, useRouter } from 'next/navigation';
 import { getPath } from '@/utils/getPath';
 import { useUserId } from '@/store/sessionStore';
 import { toast } from '@/modules/Toast';
+import { QUERY_KEY } from '@/constants/query';
 
 export const useDeleteProfileFeedItem = () => {
   const router = useRouter();
   const userId = useUserId();
   const params = useParams();
   const bookId = params.bookId as string;
+  const queryClient = useQueryClient();
 
   const { mutate: deleteProfileFeedItemMutate } = useMutation({
     mutationFn: async () => {
@@ -18,6 +20,9 @@ export const useDeleteProfileFeedItem = () => {
     },
     onSuccess: () => {
       router.replace(getPath.profile(userId!));
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEY.profileFeed, userId],
+      });
     },
     onError: () => {
       toast.error('다시 시도해주세요.');
